7effc06c048fab2dda406b2b984e81b8bc4a5d2f
unknown <you@example.com>
2022-05-16 14:19:44 +0530

    Added: Problems Based On Basic Core Programs

---
+++ string.py
@@ -0,0 +1,13 @@
+def string_replacement():
+    """
+    This method responsible for replacement of string.
+    :return: String replacement.
+    """
+    string = 'Hello <<username>>, How are you?'
+    print(string.replace("<<username>>", name))
+    return string[name]
+
+
+if __name__ == '__main__':
+    name = input("Enter your Name : ")
+    string_replacement()
